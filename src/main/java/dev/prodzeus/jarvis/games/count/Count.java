18529922688e495996483d4ff15410f788f641f9
Thor B. <synxi@outlook.dk>
2025-07-26 11:09:31 +0200

    Cloud sync.

--- src/main/java/dev/prodzeus/jarvis/games/count/Count.java
+++ src/main/java/dev/prodzeus/jarvis/games/count/Count.java
@@ -5,8 +5,8 @@ import dev.prodzeus.jarvis.configuration.enums.Channel;
 import dev.prodzeus.jarvis.enums.CollectiveMember;
 import dev.prodzeus.jarvis.enums.Counts;
 import dev.prodzeus.jarvis.enums.ServerCount;
-import dev.prodzeus.jarvis.response.Response;
 import dev.prodzeus.jarvis.utils.Utils;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -25,7 +25,7 @@ public class Count extends ListenerAdapter {
 
     private static long latestPlayer = 0L;
     private static int currentNumber;
-    private static long lastWarning = 0L;
+    private static long warningMessage = 0L;
     private static boolean highscoreAnnounced = false;
     private static int highscore;
     private static long timeOfHighscore;
@@ -49,41 +49,33 @@ public class Count extends ListenerAdapter {
     }
 
     @Override
-    public synchronized void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if (event.isWebhookMessage()) return;
-
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         final TextChannel channel = event.getChannel().asTextChannel();
         if (channel.getIdLong() != Channel.COUNT.id) return;
 
         final User user = event.getAuthor();
         if (user.isBot() || user.isSystem()) return;
 
+        final Message message = event.getMessage();
+
         try {
-            event.getMessage().delete().queueAfter(275, TimeUnit.MILLISECONDS,
+            message.delete().queueAfter(275, TimeUnit.MILLISECONDS,
                     null,
-                    f -> Jarvis.LOGGER.warn("Failed to delete message in count channel for server {}! {}", event.getGuild().getId(), f));
+                    f -> Jarvis.LOGGER.warn("Failed to delete message in count channel! {}", f));
         } catch (Exception e) {
-            Jarvis.LOGGER.warn("Failed to delete message in count channel for server {}! {}", event.getGuild().getId(), e);
+            Jarvis.LOGGER.warn("Failed to delete message in count channel! {}", e);
         }
 
         int countedNumber;
         try {
-            countedNumber = Integer.parseInt(event.getMessage().getContentRaw());
+            countedNumber = Integer.parseInt(message.getContentRaw());
         } catch (Exception ignored) {
             return;
         }
 
-        final CollectiveMember collectiveMember = Utils.getCollectiveMember(event.getMember());
+        if (!isValidPlayer(channel, user.getIdLong())) return;
 
-        if (latestPlayer == collectiveMember.id()) {
-            if (lastWarning > System.currentTimeMillis()) return;
-            else lastWarning = System.currentTimeMillis() + 15000;
-            new Response(event)
-                    .message("%s You can't count twice in a row!", Jarvis.BOT.getEmojiFormatted("red_exclamation"))
-                    .deleteAfter(15)
-                    .send();
-            return;
-        } else latestPlayer = collectiveMember.id();
+        final CollectiveMember collectiveMember = Utils.getCollectiveMember(event.getMember());
 
         final boolean gameOver = countedNumber != currentNumber;
         if (gameOver && currentNumber == 1) return;
@@ -91,32 +83,35 @@ public class Count extends ListenerAdapter {
         Jarvis.DATABASE.saveUserCounts(collectiveMember, gameOver ? 0 : 1, gameOver ? 1 : 0);
 
         final Counts counts = Jarvis.DATABASE.getUserCounts(collectiveMember);
+        if (warningMessage != 0L) {
+            channel.deleteMessageById(warningMessage).queue(s -> warningMessage = 0L);
+        }
 
-        String message;
+        String text;
         if (gameOver) {
             CountPlayer.sort();
             final int leaderboardSize = CountPlayer.getCountPlayers().size();
-            message = "# Game Over %s".formatted(Jarvis.BOT.getEmojiFormatted("lightning") +
-                                                 "\n-# Ended by: %s - %s: **%d** %s\n".formatted(collectiveMember.getMention(), "%s", currentNumber,"%s") +
-                                                 "## Leaderboard %s \n".formatted(Jarvis.BOT.getEmojiFormatted("rocket")) +
-                                                 "1. %s \n -# **%s Counts | %s**\n".formatted(
-                                                         leaderboardSize != 0 ? "<@"+CountPlayer.getCountPlayers().getFirst().id+">" : "N/A",
-                                                         leaderboardSize != 0 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
-                                                         leaderboardSize != 0 ? "%"+((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A") +
-                                                 "2. %s \n -# **%s Counts | %s**\n".formatted(
-                                                         leaderboardSize > 1 ? "<@"+CountPlayer.getCountPlayers().getFirst().id+">" : "N/A",
-                                                         leaderboardSize > 1 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
-                                                         leaderboardSize > 1 ? "%"+((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A") +
-                                                 "3. %s \n -# **%s Counts | %s**\n".formatted(
-                                                         leaderboardSize > 2 ? "<@"+CountPlayer.getCountPlayers().getFirst().id+">" : "N/A",
-                                                         leaderboardSize > 2 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
-                                                         leaderboardSize > 2 ? "%"+((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A"));
+            text = "# Game Over %s".formatted(Jarvis.BOT.getEmojiFormatted("lightning") +
+                                              "\n-# Ended by: %s - %s: **%d** %s\n".formatted(collectiveMember.getMention(), "%s", currentNumber, "%s") +
+                                              "## Leaderboard %s \n".formatted(Jarvis.BOT.getEmojiFormatted("rocket")) +
+                                              "1. %s \n -# **%s Counts | %s**\n".formatted(
+                                                      leaderboardSize != 0 ? "<@" + CountPlayer.getCountPlayers().getFirst().id + ">" : "N/A",
+                                                      leaderboardSize != 0 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
+                                                      leaderboardSize != 0 ? "%" + ((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A") +
+                                              "2. %s \n -# **%s Counts | %s**\n".formatted(
+                                                      leaderboardSize > 1 ? "<@" + CountPlayer.getCountPlayers().getFirst().id + ">" : "N/A",
+                                                      leaderboardSize > 1 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
+                                                      leaderboardSize > 1 ? "%" + ((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A") +
+                                              "3. %s \n -# **%s Counts | %s**\n".formatted(
+                                                      leaderboardSize > 2 ? "<@" + CountPlayer.getCountPlayers().getFirst().id + ">" : "N/A",
+                                                      leaderboardSize > 2 ? CountPlayer.getCountPlayers().getFirst().counts : "N/A",
+                                                      leaderboardSize > 2 ? "%" + ((CountPlayer.getCountPlayers().removeFirst().counts / currentNumber) * 100) : "N/A"));
             if (highscoreAnnounced) {
                 highscoreAnnounced = false;
-                channel.sendMessage(message.formatted("New Highscore",Jarvis.BOT.getEmojiFormatted("trophy")))
+                channel.sendMessage(text.formatted("New Highscore", Jarvis.BOT.getEmojiFormatted("trophy")))
                         .and(channel.getManager().setTopic("Server Highscore: %d".formatted(highscore)))
                         .queue(null, f -> LOGGER.warn("RestAction failed! Attempted to send new highscore message, and update topic to new highscore, but failed. {}", f));
-            } else channel.sendMessage(message.formatted("Score","")).queue();
+            } else channel.sendMessage(text.formatted("Score", "")).queue();
             currentNumber = 1;
             save();
             playerStreaks.clear();
@@ -124,20 +119,20 @@ public class Count extends ListenerAdapter {
         } else {
             CountPlayer.increment(collectiveMember.id());
             final boolean isOnStreak = computeStreak(event);
-            message = "%s **%d**%s\n-#%s Rank: %s | Correct counts: %d  |  Incorrect counts: %d".formatted(collectiveMember.getMention(), countedNumber, "%s", "%s", counts.level(), counts.correctCounts(), counts.incorrectCounts());
+            text = "%s **%d**%s\n-#%s Rank: %s | Correct counts: %d  |  Incorrect counts: %d".formatted(collectiveMember.getMention(), countedNumber, "%s", "%s", counts.level(), counts.correctCounts(), counts.incorrectCounts());
             if (countedNumber > highscore) {
-                message = message.formatted(" " + Jarvis.BOT.getEmojiFormatted("trophy"), isOnStreak ? " " + Jarvis.BOT.getEmojiFormatted("streak") : "");
+                text = text.formatted(" " + Jarvis.BOT.getEmojiFormatted("trophy"), isOnStreak ? " " + Jarvis.BOT.getEmojiFormatted("streak") : "");
                 if (!highscoreAnnounced) {
                     highscoreAnnounced = true;
-                    message = "### %s **Just broke the record! New highscore: %d** %s\n-# _Previous highscore of **%d** was made: <t:%d:R>_"
+                    text = "### %s **Just broke the record! New highscore: %d** %s\n-# _Previous highscore of **%d** was made: <t:%d:R>_"
                             .formatted(collectiveMember.getMention(), countedNumber, Jarvis.BOT.getEmojiFormatted("confetti"), highscore, timeOfHighscore);
                 }
                 highscore = countedNumber;
                 timeOfHighscore = event.getMessage().getTimeCreated().toEpochSecond();
             } else {
-                message = message.formatted(isOnStreak ? " " + Jarvis.BOT.getEmojiFormatted("streak") : "", "");
+                text = text.formatted(isOnStreak ? " " + Jarvis.BOT.getEmojiFormatted("streak") : "", "");
             }
-            channel.sendMessage(message).queue();
+            channel.sendMessage(text).queue();
         }
     }
 
@@ -148,6 +143,21 @@ public class Count extends ListenerAdapter {
         if (times.size() < 6) return false;
         times.removeFirst();
         return times.stream().allMatch(time -> (time + 60000) > compareTime);
+    }
+
+    private boolean isValidPlayer(@NotNull TextChannel channel, final long id) {
+        if (latestPlayer == id) {
+            if (warningMessage != 0L) return false;
+            channel.sendMessage("%s You can't count twice in a row!".formatted(Jarvis.BOT.getEmojiFormatted("red_exclamation")))
+                    .queue(s -> {
+                        warningMessage = s.getIdLong();
+                        s.delete().queueAfter(10, TimeUnit.SECONDS, su -> warningMessage = 0L);
+                    });
+            return false;
+        } else {
+            latestPlayer = id;
+            return true;
+        }
     }
 
     private static class CountPlayer implements Comparable<CountPlayer> {
